f4abe9a1d4c3c3ade2001ee318224267eef536a4
yogeshpoonia <49322961+yogeshpoonia@users.noreply.github.com>
2020-12-12 10:55:48 +0530

    Add files via upload

---
+++ helpme.go
@@ -0,0 +1,25 @@
+package helpme
+
+import (
+	"net/http"
+)
+
+// NewRouter helps to get new router instance.
+func getMeRouter() *Router {
+	return &Router{namedRoutes: make(map[string]*Route)}
+}
+
+type contextKey int
+
+const (
+	varsKey contextKey = iota
+	routeKey
+)
+
+// Vars returns the route variables for the current request, if any.
+func routerVar(r *http.Request) map[string]string {
+	if rv := r.Context().Value(varsKey); rv != nil {
+		return rv.(map[string]string)
+	}
+	return nil
+}
